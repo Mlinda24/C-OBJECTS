5d053b41ca5e0745936a3f8db1cbd180ae7a70e2
Mlinda24 <167519181+Mlinda24@users.noreply.github.com>
2024-04-26 01:44:46 +0200

    Add files via upload

---
+++ exercise2Q1.cpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    int number;
+    int *pnumber = new int(number);
+    string words;
+    string * pstring=new string(words);
+    cout<<"enter number\n";
+    cin>>number;
+    cout<<"enter some string\n";
+    cin>> words;
+    cout<<"your number is :"<<number<<endl;
+    cout<<"your string is :"<<words<<endl;
+
+
+
+
+
+    
+    
+    
+}
